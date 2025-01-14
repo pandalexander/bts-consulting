1e3ff1fbc9b0579ce2a1284c90910ea9607417a9
Alex Seeley <alexseeley134@gmail.com>
2025-01-14 09:19:24 -0700

    Add title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { Camera } from "lucide-react";
-
 const App = () => {
   return (
     <div className="max-w-7xl mx-auto pt-20 px-6">
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-      <Camera color="red" size={48} />
+      <h1 className="text-5xl ">Hello world!</h1>
+      <p>I am a paragraph text</p>
     </div>
   );
 };
