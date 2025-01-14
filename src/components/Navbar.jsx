5c09f424fcd95d1d66c7edea7f2704e6eaeb09c9
Alex Seeley <alexseeley134@gmail.com>
2025-01-14 11:14:39 -0700

    Add menu for mobile and drawer placeholder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,78 @@
 import Logo from "../assets/bts-logo.jpg";
 import { Phone } from "lucide-react";
-import { File } from "lucide-react";
+import { File, Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
-    <div className="sticky top-0 z-50 py-3 bg-brandDark border-b border-neutral-700/80 shadow-lg">
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-between items-center">
-          <a href="/" className="flex items-center flex-shrink-0">
-            <img src={Logo} alt="" className="h-10 w-10 mr-2" />
-            <h1 className="text-2xl tracking-tight text-white">
-              BTS&nbsp;Consulting
-            </h1>
-          </a>
-          <ul className="hidden lg:flex ml-14 space-x-12 text-white ">
-            <li>
-              <a href="#" className="hover:text-sky-300">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-sky-300">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-sky-300">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a
-              href="#"
-              className="py-2 px-3 border-0 rounded-md bg-gradient-to-br from-sky-700 to-sky-500 text-white hover:from-sky-600 hover:to-sky-400"
-            >
-              <div className="flex space-x-2 items-center justify-center">
-                <File color="white" size={20} />
-                <p>Send Documents</p>
-              </div>
+    <div>
+      <div className="sticky top-0 z-50 py-3 bg-brandDark border-b border-neutral-700/80 shadow-lg">
+        <div className="container px-4 mx-auto relative text-sm">
+          <div className="flex justify-between items-center ">
+            <a href="/" className="flex items-center flex-shrink-0 ">
+              <img src={Logo} alt="" className="h-10 w-10 mr-2" />
+              <h1 className="text-2xl tracking-tight text-white">
+                BTS&nbsp;Consulting
+              </h1>
             </a>
-            <a
-              href="tel:1-719-465-2465"
-              className="py-2 px-3 border-0 rounded-md bg-gradient-to-br from-sky-700 to-sky-500 text-white hover:from-sky-600 hover:to-sky-400"
-            >
-              <div className="flex space-x-2 items-center justify-center">
-                <Phone color="white" size={20} />
-                <p>(719) 465-2465</p>
-              </div>
-            </a>
+            <div className="lg:hidden flex text-white">
+              {menuIsOpen ? (
+                <X onClick={handleMenuClick} />
+              ) : (
+                <Menu onClick={handleMenuClick} />
+              )}
+            </div>
+            <ul className="hidden lg:flex ml-14 space-x-12 text-white  ">
+              <li>
+                <a href="#" className="hover:text-sky-300">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-sky-300">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-sky-300">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <div className="hidden lg:flex justify-center space-x-12 items-center">
+              <a
+                href="#"
+                className="py-2 px-3 border-0 rounded-md bg-gradient-to-br from-sky-700 to-sky-500 text-white hover:from-sky-600 hover:to-sky-400"
+              >
+                <div className="flex space-x-2 items-center justify-center">
+                  <File color="white" size={20} />
+                  <p>Send Documents</p>
+                </div>
+              </a>
+              <a
+                href="tel:1-719-465-2465"
+                className="py-2 px-3 border-0 rounded-md bg-gradient-to-br from-sky-700 to-sky-500 text-white hover:from-sky-600 hover:to-sky-400"
+              >
+                <div className="flex space-x-2 items-center justify-center">
+                  <Phone color="white" size={20} />
+                  <p>(719) 465-2465</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
+      {menuIsOpen && (
+        <div className="bg-amber-400 lg:hidden fixed w-full flex flex-col right-0 z-20 p-12 justify-center items-center">
+          <h1 className="text-white">I am open!</h1>
+        </div>
+      )}
     </div>
   );
 };
