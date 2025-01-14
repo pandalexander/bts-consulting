7fe2977358d9cb59f35827b2915922952affc984
Alex Seeley <alexseeley134@gmail.com>
2025-01-14 09:13:34 -0700

    Create boilerplate react tailwind vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Camera } from "lucide-react";
+
+const App = () => {
+  return (
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+      <Camera color="red" size={48} />
+    </div>
+  );
+};
+
+export default App;
