8fb1a73758349b1202c58c41f51cb0297dcad21c
Alex <alexseeley134@gmail.com>
2025-01-14 21:39:30 -0700

    Fix sizing and text balance

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ const Hero = () => {
   return (
     <div>
       <div className="flex flex-col items-center mt-6 lg:mt 20">
-        <h1 className="text-5xl sm:text-7xl lg:text-8xl text-center tracking-wide ">
+        <h1 className="text-6xl sm:text-8xl lg:text-8xl text-center tracking-wide ">
           The Taxpayer’s{" "}
           <span className="bg-gradient-to-br from-amber-300 to-amber-700 bg-clip-text text-transparent">
             Champion
           </span>
         </h1>
-        <h2 className="font-semibold p-4 lg:mt-4 text-xl sm:text-3xl lg:text-4xl text-center tracking-tight bg-gradient-to-r from-sky-400 to-sky-700 bg-clip-text text-transparent">
+        <h2 className="text-balance font-semibold p-4 lg:mt-4 text-xl sm:text-3xl lg:text-4xl text-center tracking-tight bg-gradient-to-r from-sky-400 to-sky-700 bg-clip-text text-transparent">
           Expert Tax & Accounting Services Since 2010
         </h2>
       </div>
