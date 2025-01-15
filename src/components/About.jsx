e4f5762415ca842057e88371d0a33376f1d80619
Alex Seeley <alexseeley134@gmail.com>
2025-01-15 15:46:37 -0700

    Build out Services page with images

---
+++ src/components/About.jsx
@@ -0,0 +1,92 @@
+import IrsColorIcon from "../assets/irs-color.jpg";
+import QuickbooksIcon from "../assets/quickbooks.png";
+import GoogleReviews from "../assets/google-review.png";
+
+const About = () => {
+  return (
+    <div className="">
+      <div className="">
+        <div className="relative border-b border-brandDark min-h-[800px] ">
+          <div className="text-center">
+            <h2 className="font-specialSerif text-5xl sm:text-6xl lg:text-7xl tracking-wide text-balance">
+              About BTS Consulting
+            </h2>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="text-lg p-6 flex-1 space-y-5">
+              <p>
+                Founded in 2010 in the serene setting of Volcano, Hawaii, BTS
+                Consulting has grown out of a legacy of resilience and
+                dedication. Its story begins with Kate and her late husband,
+                Fritz, who established Tara Firma Inn - Volcano, a charming bed
+                & breakfast located near the iconic Hawaii Volcanoes National
+                Park. To support their growing ventures, Fritz, a master
+                carpenter, launched his business, &quot;Fritz the
+                Carpenter,&quot; while Kate provided tax and accounting services
+                to small businesses on the Big Island.
+              </p>
+              <p>
+                After Fritz’s passing in 2013, Kate continued to run the bed &
+                breakfast until 2016 before relocating to Georgia with the
+                support of her family. During her time in Greensboro and Athens,
+                Kate honed her expertise in finance and business consulting,
+                paving the way for new opportunities. In Colorado Springs, a
+                place filled with fond memories of visiting her father, Kate
+                found her perfect home to continue the success of BTS
+                Consulting.
+              </p>
+              <p>
+                Today, BTS Consulting thrives in Colorado Springs, dedicated to
+                empowering small businesses with expert financial guidance and
+                personalized service.
+              </p>
+              <div className="bg-zinc-100 rounded-lg p-6 flex items-end justify-evenly flex-wrap space-y-8 lg:space-y-2">
+                <div className="flex flex-col justify-center items-center rounded-lg max-w-60">
+                  <a
+                    href="https://maps.app.goo.gl/UTLbG8jH1U3bMUi28"
+                    target="_blank"
+                  >
+                    {" "}
+                    <img src={GoogleReviews} alt="" className="w-full" />
+                    <p className="font-thin text-zinc-600 text-center">
+                      Consistent 5 star reviews
+                    </p>
+                  </a>
+                </div>
+
+                <a href="https://quickbooks.intuit.com/" target="_blank">
+                  <div className="flex flex-col justify-center items-center rounded-lg">
+                    {" "}
+                    <img src={QuickbooksIcon} alt="" className="max-w-28" />
+                    <p className="font-thin text-zinc-600 mt-4 text-balance text-center">
+                      QuickBooks ProAdvisor Silver Tier
+                    </p>
+                  </div>
+                </a>
+
+                <a
+                  href="https://www.irs.gov/tax-professionals/enrolled-agents/enrolled-agent-information#:~:text=Enrolled%20agent%20status%20is%20the,)%2C%20have%20unlimited%20practice%20rights."
+                  target="_blank"
+                >
+                  <div className="flex flex-col justify-center items-center rounded-lg max-w-80">
+                    <img
+                      src={IrsColorIcon}
+                      alt=""
+                      className="w-full rounded-lg"
+                    />
+                    <p className="font-thin text-zinc-600 mt-4 text-center">
+                      IRS enrolled agent
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar.jsx";
 import Hero from "./Hero.jsx";
 import Services from "./Services.jsx";
+import About from "./About.jsx";
 
 import { Element } from "react-scroll";
 
@@ -13,28 +14,13 @@ const App = () => {
 
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
-        <div className="mt-[1000px]">I am bottom</div>
+        <div className="mt-20"></div>
 
         <Element name="about" className="pt-16">
-          {/* Content for Section 1 */}
-
-          <section id="section1" className="mb-[1000px]">
-            <h2 className="mb-24">Section 1: About</h2>
-            <p className="">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet.
-            </p>
-            <div className="">
-              <ul className="">
-                <li className="">Phasellus dapibus</li>
-                <li className="">Nulla quis lorem</li>
-                <li className="">Maecenas feugiat</li>
-                <li className="">Vivamus luctus</li>
-              </ul>
-            </div>
-          </section>
+          <About />
         </Element>
+
+        <div className="mt-16"></div>
 
         <Element name="services" className="pt-16">
           <Services />
