ef46ee408de73b71124d51201b83532e5ac88410
Alex <alexseeley134@gmail.com>
2025-01-14 19:28:38 -0700

    Finish colorings and sizing hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,15 @@ const Hero = () => {
   return (
     <div>
       <div className="flex flex-col items-center mt-6 lg:mt 20">
-        <h1 className="text-5xl sm:text-7xl lg:text-8xl text-center tracking-wide bg-gradient-to-r from-sky-500 to-sky-700 bg-clip-text text-transparent">
-          The Taxpayer’s Champion
+        <h1 className="text-5xl sm:text-7xl lg:text-8xl text-center tracking-wide ">
+          The Taxpayer’s{" "}
+          <span className="bg-gradient-to-br from-amber-300 to-amber-700 bg-clip-text text-transparent">
+            Champion
+          </span>
         </h1>
-        <h1 className="mt-4 lg:mt-8 text-2xl sm:text-4xl lg:text-5xl text-center tracking-tight ">
+        <h2 className="font-semibold p-4 lg:mt-4 text-xl sm:text-3xl lg:text-4xl text-center tracking-tight bg-gradient-to-r from-sky-400 to-sky-700 bg-clip-text text-transparent">
           Expert Tax & Accounting Services Since 2010
-        </h1>
+        </h2>
       </div>
     </div>
   );
