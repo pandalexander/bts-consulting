f178c7c79f4f47f0cbe727acb1fe64f406734aaa
Alex <alexseeley134@gmail.com>
2025-01-14 19:06:04 -0700

    Add hero text and background gradient

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
 
 const App = () => {
   return (
@@ -6,45 +7,7 @@ const App = () => {
       <Navbar />
 
       <div className="max-w-7xl mx-auto pt-20 px-6">
-        <h1 className="text-5xl">Hello world!</h1>
-        <p>I am a paragraph text</p>
-        <p className="text-blue-500">This text should be blue</p>
-        <h1 className="text-3xl">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Hendrerit gravida
-          habitant curabitur porttitor lacinia natoque odio. Curae ad metus
-          phasellus magnis consectetur tempus dolor purus. Maximus sollicitudin
-          risus venenatis phasellus lobortis nullam auctor fermentum. Felis
-          metus morbi purus duis facilisis viverra molestie consectetur. Eu
-          cubilia libero; accumsan nam scelerisque augue. Ultrices luctus ad
-          nascetur fames odio eleifend lectus. Finibus imperdiet efficitur
-          condimentum dui dapibus congue. Dapibus dictum conubia dui augue nibh.
-          Porta at iaculis ante fringilla, eros purus? Adipiscing libero magnis;
-          parturient interdum eros finibus est lectus. Mollis mollis potenti
-          ullamcorper malesuada vel iaculis! Ligula tristique fusce ullamcorper
-          curabitur, tempus senectus bibendum nostra. Adipiscing nostra neque
-          bibendum diam commodo montes felis erat. Feugiat lorem mollis at, erat
-          porttitor convallis. Torquent placerat eleifend mattis rhoncus sit
-          feugiat augue ullamcorper. Erat ultricies massa tincidunt diam dis
-          posuere. Phasellus pretium iaculis integer sollicitudin duis ipsum
-          eleifend. Sed gravida conubia platea, feugiat consectetur nascetur. Aa
-          in aenean rutrum pulvinar suscipit ultrices fermentum. Natoque
-          condimentum fames consectetur leo habitant curae sociosqu conubia!
-          Torquent gravida elit dis dis nulla metus penatibus. Tristique mauris
-          inceptos ultricies fusce faucibus turpis curabitur sagittis. Viverra
-          dictum nullam dolor rhoncus leo platea senectus fringilla. Lectus id
-          ad primis odio mattis penatibus. Tincidunt ridiculus turpis efficitur
-          sit laoreet interdum faucibus dis. Maecenas facilisi arcu pretium
-          himenaeos elementum consectetur semper aenean. Nam bibendum non platea
-          aptent dui pellentesque dictum sit. Arcu convallis metus ad consequat
-          duis iaculis arcu nunc pellentesque. Finibus sollicitudin inceptos
-          tellus luctus rutrum; semper cras libero. Velit dictumst fringilla
-          potenti libero velit etiam lobortis. Phasellus massa tincidunt
-          sollicitudin purus habitant ornare integer aliquam. Commodo dolor
-          dignissim vivamus non inceptos maximus odio. Massa leo bibendum cursus
-          condimentum; sociosqu diam libero nisl lorem. In vitae lectus torquent
-          hac purus dictum ante. Magnis auctor neque nisl nunc massa sed fames
-          ac mollis. Nibh metus vitae egestas urna ipsum ultricies nulla dolor.
-        </h1>
+        <Hero />
       </div>
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-6 lg:mt 20">
+        <h1 className="text-5xl sm:text-7xl lg:text-8xl text-center tracking-wide bg-gradient-to-r from-sky-500 to-sky-700 bg-clip-text text-transparent">
+          The Taxpayer’s Champion
+        </h1>
+        <h1 className="mt-4 lg:mt-8 text-2xl sm:text-4xl lg:text-5xl text-center tracking-tight ">
+          Expert Tax & Accounting Services Since 2010
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
